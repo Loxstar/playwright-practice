96ba5c7b37884359520fe06764d0aa7a79fa0ae7
loxstar <loxxstar@hotmail.com>
2025-11-11 13:37:43 -0500

    Added state storage file for session storage

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,9 +27,10 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: process.env.TEST,
+    storageState: 'state.json',
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
+    headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
